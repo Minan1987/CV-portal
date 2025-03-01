33cf02c2a52ef694295b000852daa4b0f04abc92
Mina Nazari <mina.nazary66@gmail.com>
2025-03-01 13:44:33 +0330

    sample works added to data.

--- src/components/data/sampleWorkData.jsx
+++ src/components/data/sampleWorkData.jsx
@@ -2,7 +2,7 @@ export const sampleWorks = [
     { 
       title: "توسعه یک پروژه کوچک مدیریت مخاطبین با استفاده از کتابخانه React", 
       image: "/images/blog1.jpg", 
-      link: "https://contactmanagement.graphicalweb.ir",
+      link: "https://contact.graphicalweb.ir",
       date: "اردیبهشت ۱۴۰۳" 
     },
     { 
@@ -16,5 +16,77 @@ export const sampleWorks = [
       image: "/images/blog3.jpg", 
       link: "https://minan1987.github.io/industrial/",
       date: "آبان ۱۴۰۳" 
+    },
+    { 
+      title: "طرحی و پیاده سازی رابط کاربری (UI/UX) رسانه نوا (nava.ir)", 
+      image: "/images/blog4.jpg", 
+      link: "https://navatv.com/",
+      date: "آبان ۱۳۹۷" 
+    },
+    { 
+      title: "طرحی رابط کاربری (UI/UX) وبسایت کلیک (click.ir)", 
+      image: "/images/blog8.jpg", 
+      link: "https://www.click.ir/",
+      date: "خرداد ۱۳۹۵" 
+    },
+    { 
+      title: "طرحی و پیاده سازی رابط کاربری (UI/UX) تی وی پلاس (plus.ir)", 
+      image: "/images/blog5.jpg", 
+      link: "https://plus.ir/",
+      date: "آبان ۱۳۹۶" 
+    },
+    { 
+      title: "طرحی و پیاده سازی رابط کاربری (UI/UX) وبسایت آشپزی (ashpazi.ir)", 
+      image: "/images/blog6.jpg", 
+      link: "https://ashpazi.graphicalweb.ir/",
+      date: "اردیبهشت ۱۳۹۶" 
+    },
+    { 
+      title: "طرحی و پیاده سازی رابط کاربری (UI/UX) وبسایت الوان پرند (https://alvanparand.com/)", 
+      image: "/images/blog9.jpg", 
+      link: "https://alvanparand.com/",
+      date: "آبان ۱۳۹۵" 
+    },
+    { 
+      title: "طرحی رابط کاربری (UI/UX) وبسایت rahnamoonco.com", 
+      image: "/images/blog7.jpg", 
+      link: "https://www.rahnamoon.ir/",
+      date: "شهریور ۱۳۹۶" 
+    },
+    { 
+      title: "طرحی رابط کاربری (UI/UX) وبسایت sadraco.com", 
+      image: "/images/blog10.jpg", 
+      link: "https://sadraco.com/",
+      date: "آذر ۱۳۹۵" 
+    },
+    { 
+      title: "طرحی رابط کاربری (UI/UX) وبسایت اسکناس ایرانی", 
+      image: "/images/blog11.jpg", 
+      link: "http://eskenaseirani.com/fa/index.asp?p=pages&id=1",
+      date: "آذر ۱۳۹۵" 
+    },
+    { 
+      title: "طرحی رابط کاربری (UI/UX) وبسایت شرکت فابکو (fubco.ir) ", 
+      image: "/images/blog12.jpg", 
+      link: "https://www.fubco.ir/",
+      date: "دی ۱۳۹۵" 
+    },
+    { 
+      title: "طرحی رابط کاربری (UI/UX) وبسایت شرکت حرفه و فن (herfofan.com) ", 
+      image: "/images/blog13.jpg", 
+      link: "http://www.herfofan.com/",
+      date: "بهمن ۱۳۹۵" 
+    },
+    { 
+      title: "طرحی رابط کاربری (UI/UX) وبسایت شرکت شالوده آب (shaloodehab.ir) ", 
+      image: "/images/blog14.jpg", 
+      link: "https://shaloodehab.ir/",
+      date: "اسفند ۱۳۹۵" 
+    },
+    { 
+      title: "طرحی رابط کاربری (UI/UX) فروشگاه ایران زمین (irzamin.com) ", 
+      image: "/images/blog15.jpg", 
+      link: "https://irzamin.com/",
+      date: "اسفند ۱۳۹۴" 
     },
   ];
